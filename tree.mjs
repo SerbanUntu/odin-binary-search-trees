5b56311fed9e1a8b2dcd705b4fd17f8bc230933e
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2024-07-09 17:54:37 +0300

    feat: tree manipulation operations

--- tree.mjs
+++ tree.mjs
@@ -52,4 +52,97 @@ export class Tree {
 	print() {
 		prettyPrint(this.root)
 	}
+
+	insert(value) {
+		const newNode = new Node(value)
+		let currentNode = this.root
+		if (currentNode === null) {
+			this.root = newNode
+			return
+		}
+		while (true) {
+			if (newNode.data < currentNode.data) {
+				if (currentNode.left === null) {
+					currentNode.left = newNode
+					return
+				}
+				currentNode = currentNode.left
+			} else if (newNode.data > currentNode.data) {
+				if (currentNode.right === null) {
+					currentNode.right = newNode
+					return
+				}
+				currentNode = currentNode.right
+			} else return
+		}
+	}
+
+	delete(value) {
+		let currentNode = this.root
+		let parentNode = currentNode
+		let pathTaken = 'left'
+		while (currentNode !== null) {
+			if (currentNode.data === value) {
+				if (currentNode.left && currentNode.right) {
+					// Two children
+					let lastReplacement = currentNode
+					let replacementPathTaken = 'right'
+					let replacement = currentNode.right
+					while (replacement.left) {
+						lastReplacement = replacement
+						replacementPathTaken = 'left'
+						replacement = replacement.left
+					}
+					currentNode.data = replacement.data
+					if (replacement.right) {
+						lastReplacement[replacementPathTaken] = replacement.right
+					} else {
+						lastReplacement[replacementPathTaken] = null
+					}
+				} else if (currentNode.left) {
+					// Left child
+					if (currentNode.data === this.root.data) {
+						this.root = this.root.left
+						return true
+					}
+					parentNode[pathTaken] = currentNode.left
+				} else if (currentNode.right) {
+					// Right child
+					if (currentNode.data === this.root.data) {
+						this.root = this.root.right
+						return true
+					}
+					parentNode[pathTaken] = currentNode.right
+				} else {
+					// No children
+					if (currentNode.data === this.root.data) {
+						this.root = null
+						return true
+					}
+					parentNode[pathTaken] = null
+				}
+				return true
+			}
+			parentNode = currentNode
+			if (currentNode.data > value) {
+				pathTaken = 'left'
+				currentNode = currentNode.left
+			}
+			if (currentNode.data < value) {
+				pathTaken = 'right'
+				currentNode = currentNode.right
+			}
+		}
+		return false
+	}
+
+	find(value) {
+		let currentNode = this.root
+		while (currentNode !== null) {
+			if (currentNode.data === value) return currentNode
+			if (currentNode.data > value) currentNode = currentNode.left
+			if (currentNode.data < value) currentNode = currentNode.right
+		}
+		return currentNode
+	}
 }
--- tree.test.mjs
+++ tree.test.mjs
@@ -1,18 +1,22 @@
 import { Tree } from './tree.mjs'
 
 const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 10, 324])
+test.insert(65)
+console.log(test.delete(4)) // true
 test.print()
 /*
 │           ┌── 6345
 │       ┌── 324
 │       │   └── 67
+│       │       └── 65
 │   ┌── 23
 │   │   │   ┌── 10
 │   │   └── 9
 └── 8
-    │       ┌── 7
-    │   ┌── 5
-    └── 4
+    │   ┌── 7
+    └── 5
         │   ┌── 3
         └── 1
 */
+console.log(test.find(5)) // Node { data: 5, left: [Node], right: [Node] }
+console.log(test.find(111)) // null
